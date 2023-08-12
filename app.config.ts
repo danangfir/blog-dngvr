8e95c442993ec2148a84bc40ba279a2de1960348
danangfir <danangfirmino26@gmail.com>
2023-08-12 23:18:13 +0700

    changes

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
+    title: 'Danang',
     description: 'The minimalist blog theme',
     image: {
       src: '/social-card-preview.png',
